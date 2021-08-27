feaad23886f8a9c3a6b3314082d2f7c5de944c63
Mansour Behabadi <mansour@oxplot.com>
2021-08-28 03:32:45 +1000

    Some GUI work

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"sort"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/oxplot/raspberrypi-archlinux-installer/disk"
@@ -11,53 +13,101 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
 )
 
 const (
-	title = "Raspberry Pi Arch Linux Installer"
+	title      = "Raspberry Pi Arch Linux Installer"
+	confirmMsg = "You are about to DESTROY ALL DATA on\n\n%s\n\nThere is no going back.\nAre you sure?"
 )
 
 var (
+	mainWin fyne.Window
 	sdCards = widget.NewSelect(nil, nil)
+
+	disks = struct {
+		mu    sync.Mutex
+		items []disk.Disk
+	}{}
 )
 
 func refreshSDCards() {
 Outer:
 	for {
 		time.Sleep(time.Second)
-		dd, err := disk.Get()
+
+		ds, err := disk.Get()
 		if err != nil {
 			log.Printf("error: cannot get list of disks: %s", err)
-			continue
 		}
 
-		opts := make([]string, len(dd))
-		for i, d := range dd {
-			opts[i] = d.Name()
-		}
-		sort.Slice(opts, func(i, j int) bool {
-			return strings.ToLower(opts[i]) < strings.ToLower(opts[j])
+		sort.Slice(ds, func(i, j int) bool {
+			return strings.ToLower(ds[i].Name()) < strings.ToLower(ds[j].Name())
 		})
-		sdCards.Options = opts
-		for _, o := range opts {
+		names := make([]string, len(ds))
+		for i, d := range ds {
+			names[i] = d.Name()
+		}
+
+		disks.mu.Lock()
+		disks.items = ds
+		sdCards.Options = names
+		for _, o := range names {
 			if o == sdCards.Selected {
+				disks.mu.Unlock()
 				continue Outer
 			}
 		}
 		sdCards.ClearSelected()
+		disks.mu.Unlock()
 	}
+}
+
+func install(d disk.Disk) error {
+	cancelled := make(chan bool)
+	prog := widget.NewProgressBar()
+	prog.Min, prog.Max = 0, 100
+	progDiag := dialog.NewCustom("Installing", "Cancel", prog, mainWin)
+	progDiag.SetOnClosed(func() {
+		close(cancelled)
+	})
+	progDiag.Show()
+
+	<-cancelled
+
+	return fmt.Errorf("shit went to hell")
 }
 
 func main() {
 	log.SetFlags(0)
 	a := app.New()
-	w := a.NewWindow(title)
+	mainWin = a.NewWindow(title)
 
 	install := widget.NewButton("Install", func() {
+		disks.mu.Lock()
+		selIdx := sdCards.SelectedIndex()
+		if selIdx == -1 {
+			disks.mu.Unlock()
+			return
+		}
+		d := disks.items[selIdx]
+		disks.mu.Unlock()
+
+		dialog.ShowConfirm("Are you sure?", fmt.Sprintf(confirmMsg, d.Name()), func(y bool) {
+			if !y {
+				return
+			}
+			go func() {
+				if err := install(d); err != nil {
+					dialog.ShowInformation("Error", err.Error(), mainWin)
+				}
+			}()
+		}, mainWin)
 	})
 	install.Disable()
+
 	wifiName := widget.NewEntry()
 	wifiPassword := widget.NewPasswordEntry()
 	wifiEnable := widget.NewCheck("Enable WiFi", func(checked bool) {
@@ -69,15 +119,17 @@ func main() {
 			wifiPassword.Disable()
 		}
 	})
+	wifiEnable.OnChanged(false)
+
 	sdCards.OnChanged = func(s string) {
-		if sdCards.SelectedIndex() == -1 {
+		if s == "" {
 			install.Disable()
 		} else {
 			install.Enable()
 		}
 	}
 
-	w.SetContent(container.NewPadded(container.NewVBox(
+	mainWin.SetContent(container.NewPadded(container.NewVBox(
 		widget.NewLabelWithStyle(title, fyne.TextAlignLeading, fyne.TextStyle{Bold: true}),
 		widget.NewSeparator(),
 		container.NewBorder(nil, nil, widget.NewLabel("SD Card:"), nil, sdCards),
@@ -90,8 +142,9 @@ func main() {
 		install,
 	)))
 
-	wifiEnable.OnChanged(false)
+	contentSize := mainWin.Content().Size()
+	mainWin.Resize(fyne.Size{contentSize.Width * 2, contentSize.Height * 2})
 
 	go refreshSDCards()
-	w.ShowAndRun()
+	mainWin.ShowAndRun()
 }
